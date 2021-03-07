848c7f16c043aee1ec84468bcd63f8c7d8a1eec4
Arthur Gabriel Cipitelli <cipitelliarthurgabriel@gmail.com>
2021-03-07 00:37:30 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,4 @@ sexo = str(input('Sexo [M/F]: ')).strip().upper()[0]
 idade = int(input('Idade: '))
 altura = float(input('Altura (m): '))
 peso = float(input('Peso (Kg):'))
-
-print(f'Seu sexo é {sexo}')
-print(f'Sua idade é {idade} anos')
-print(f'Sua altura é {altura}m')
-print(f'Seu peso é {peso}Kg')
+imc = peso / (altura ** 2)

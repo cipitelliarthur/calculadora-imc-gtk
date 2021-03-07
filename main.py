e530f4a622275ad1adb2e6e054bafb49aa6a9133
Arthur Gabriel Cipitelli <cipitelliarthurgabriel@gmail.com>
2021-03-07 17:11:51 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,33 @@
 #Ainda falta acrescentar a diferença entre idade e sexo
 
-sexo = str(input('Sexo [M/F]: ')).strip().upper()[0]
-idade = int(input('Idade: '))
-altura = float(input('Altura (m): '))
-peso = float(input('Peso (Kg):'))
-imc = peso / (altura ** 2)
+while True:
+    option = str(input('Quer calcular outro IMC? [S/N]: ')).strip().upper()[0]
 
-print(f'Seu IMC foi de {imc:.2f} Kg/m²')
+    if option == 'N':
+        break
+    else:
+        sexo = str(input('Sexo [M/F]: ')).strip().upper()[0]
+        idade = int(input('Idade: '))
+        altura = float(input('Altura (m): '))
+        peso = float(input('Peso (Kg):'))
+        imc = peso / (altura ** 2)
 
-if sexo == 'M':
-    if imc < 16.9:
-        print('Muito abaixo do peso')
-    elif imc < 18.49:
-        print('Abaixo do peso')
-    elif imc < 24.99:
-        print('Peso normal')
-    elif imc < 29.99:
-        print('Acima do peso')
-    elif imc < 34.99:
-        print('Obesidade Grau I')
-    elif imc < 39.99:
-        print('Obesidade Severa Grau II ')
-    elif imc > 40.00:
-        print('Obesidade Mórbida Grau III ')
-if sexo == 'F':
-    print('Sexo feminino')
+        print(f'Seu IMC foi de {imc:.2f} Kg/m²')
+
+        if sexo == 'M':
+            if imc < 16.9:
+                print('Muito abaixo do peso')
+            elif imc < 18.49:
+                print('Abaixo do peso')
+            elif imc < 24.99:
+                print('Peso normal')
+            elif imc < 29.99:
+                print('Acima do peso')
+            elif imc < 34.99:
+                print('Obesidade Grau I')
+            elif imc < 39.99:
+                print('Obesidade Severa Grau II ')
+            elif imc > 40.00:
+                print('Obesidade Mórbida Grau III ')
+        if sexo == 'F':
+            print('Sexo feminino')

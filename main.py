90eb52d5230464aa2f04133144dc1671b59d1f15
Arthur Gabriel Cipitelli <cipitelliarthurgabriel@gmail.com>
2021-03-07 00:35:56 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,8 @@ sexo = str(input('Sexo [M/F]: ')).strip().upper()[0]
 idade = int(input('Idade: '))
 altura = float(input('Altura (m): '))
 peso = float(input('Peso (Kg):'))
+
+print(f'Seu sexo é {sexo}')
+print(f'Sua idade é {idade} anos')
+print(f'Sua altura é {altura}m')
+print(f'Seu peso é {peso}Kg')
